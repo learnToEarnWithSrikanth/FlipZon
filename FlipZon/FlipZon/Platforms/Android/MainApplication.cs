6f10b314f75cf4178e09185d3570f52ff75033e0
learnToEarnWithSrikanth <148522390+learnToEarnWithSrikanth@users.noreply.github.com>
2023-12-04 11:39:53 +0530

    Update MainApplication.cs

--- FlipZon/FlipZon/Platforms/Android/MainApplication.cs
+++ FlipZon/FlipZon/Platforms/Android/MainApplication.cs
@@ -14,9 +14,7 @@ public class MainApplication : MauiApplication
 
     protected override MauiApp CreateMauiApp()
     {
-        raw.SetProvider(new SQLite3Provider_e_sqlite3());
         return MauiProgram.CreateMauiApp();
-
     }
 }
 
