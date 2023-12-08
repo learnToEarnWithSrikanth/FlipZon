ec4349f6873756652c14cb77c00ecf29d98f1635
learnToEarnWithSrikanth <148522390+learnToEarnWithSrikanth@users.noreply.github.com>
2023-12-08 11:09:35 +0530

    Fixed the android back button not working

--- FlipZon/FlipZon/Platforms/Android/MainActivity.cs
+++ FlipZon/FlipZon/Platforms/Android/MainActivity.cs
@@ -1,33 +1,51 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 using Controls.UserDialogs.Maui;
 
-namespace MauiSampleTest;
-
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-[IntentFilter(new[] { Platform.Intent.ActionAppAction },
-              Categories = new[] { global::Android.Content.Intent.CategoryDefault })]
-public class MainActivity : MauiAppCompatActivity
+namespace MauiSampleTest
 {
-    protected override void OnResume()
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter(new[] { Platform.Intent.ActionAppAction },
+                  Categories = new[] { global::Android.Content.Intent.CategoryDefault })]
+    public class MainActivity : MauiAppCompatActivity
     {
-        base.OnResume();
+        private readonly INavigationService _navigationService;
 
-        Platform.OnResume(this);
+        public MainActivity()
+        {
+           
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            Platform.OnResume(this);
+        }
+
+        protected override void OnNewIntent(Android.Content.Intent intent)
+        {
+            base.OnNewIntent(intent);
+
+            Platform.OnNewIntent(intent);
+        }
+
+        public override async void OnBackPressed()
+        {
+            var currentScreen =  App.Current.MainPage.Navigation.NavigationStack.LastOrDefault();
+            if (currentScreen is HomeScreen)
+                await ShowCloseAppConfirmationScreen();
+            else
+                await App.Current.MainPage.Navigation.PopAsync();
+
+        }
+
+        private async Task ShowCloseAppConfirmationScreen()
+        {
+            var response = await UserDialogs.Instance.ConfirmAsync("Would like to close the app", "Close the app", "Yes", "No");
+            if (response)
+                Finish();
+
+        }
     }
-
-    protected override void OnNewIntent(Android.Content.Intent intent)
-    {
-        base.OnNewIntent(intent);
-
-        Platform.OnNewIntent(intent);
-    }
-    //protected override void OnCreate(Bundle bundle)
-    //{
-    //    base.OnCreate(bundle);
-    //   // PlatformConfig.Instance.ParentWindow = this;
-    //   // UserDialogs.UseUserDialogs;
-    //}
 }
-
--- FlipZon/FlipZon/PrismStartup.cs
+++ FlipZon/FlipZon/PrismStartup.cs
@@ -5,9 +5,27 @@ internal static class PrismStartup
 {
     public static void Configure(PrismAppBuilder builder)
     {
+       
         builder.RegisterTypes(RegisterTypes)
-                .OnAppStart("NavigationPage/HomeScreen");
+              .OnAppStart("NavigationPage/HomeScreen");
+      
+
     }
+    //private static Page DetermineMainPage()
+    //{
+    //    if (Preferences.ContainsKey(Constants.USER_ID))
+    //    {
+    //        var id = Preferences.Get(Constants.USER_ID, -1);
+    //        if (id != -1)
+    //        {
+    //            // User is logged in, navigate to HomeScreen using Prism
+    //            return new NavigationPage(new HomeScreen());
+    //        }
+    //    }
+
+    //    // User is not logged in or user ID is not found, navigate to LoginScreen using Prism
+    //    return new NavigationPage(new LoginScreen());
+    //}
 
     private static void RegisterTypes(IContainerRegistry containerRegistry)
     {
@@ -21,5 +39,6 @@ internal static class PrismStartup
         containerRegistry.RegisterForNavigation<AddressListScreen, AddressListScreenViewModel>();
         containerRegistry.RegisterForNavigation<AddAddressScreen, AddAddressScreenViewModel>();
     }
+    
 }
 
